965de8eeff5170cc86130a9c74521c86bb54b133
Varun Yadav <vy056038@gmail.com>
2024-02-23 16:14:31 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -10,17 +10,17 @@ function toggleMenu(){
 const text=document.querySelector(".sec-text");
 const textload = ()=>{
     setTimeout( ()=>{
+        text.innerHTML="MERN Stack Developer"
+    }, 0)
+    setTimeout( ()=>{
         text.innerHTML="Programer";
     }, 4000)
     setTimeout( ()=>{
-        text.innerHTML="MERN Stack Developer"
+        text.innerHTML="Problem Solver";
     }, 8000)
     setTimeout( ()=>{
-        text.innerHTML="Problem Solver";
-    }, 12000)
-    setTimeout( ()=>{
         text.innerHTML="HardCore DSA Lover";
-    }, 16000)
+    }, 12000)
 }
 textload();
 setInterval(textload,12000);
